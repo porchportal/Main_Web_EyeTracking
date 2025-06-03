c38886fb753889a50638fe2bc5233561ffa25fb2
Varanpong Suthiponpisal <porchportal4@gmail.com>
2025-06-04 01:31:56 +0700

    update next config for docker build

--- main-web/frontend/.next/server/middleware-build-manifest.js
+++ main-web/frontend/.next/server/middleware-build-manifest.js
@@ -24,11 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/collected-dataset-customized": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/collected-dataset-customized.js"
     ]
   },
   "ampFirstPages": []
